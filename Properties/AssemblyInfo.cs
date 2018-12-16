c4c4da0a27bc01438620745f44f5a8297f56d4e6
Sun <22406854+SunTheCourier@users.noreply.github.com>
2018-12-15 21:43:02 -0800

    Update AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Copyright ©  2018 SunTheCourier")]
+[assembly: AssemblyCopyright("Copyright © 2018 SunTheCourier")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
